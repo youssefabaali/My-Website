1556b0e55d7b136a6ff3f3a667e6cbcf89cabed7
youssef <joseph.abaali@gmail.com>
2026-09-06 15:04:24 +0200

    commit

--- src/defaultData.ts
+++ src/defaultData.ts
@@ -3927,7 +3927,7 @@ export const defaultSiteData: CMSSiteData = {
     }
   },
   "linkPreview": {
-    "shareImage": "https://www.youssefabaali.com/assets/images/link-share-preview-1788695247100.jpg",
+    "shareImage": "https://www.youssefabaali.com/assets/images/link-share-preview-1788699620740.png",
     "shareTitle": "Youssef Abaali — Motion Graphics Designer",
     "shareDescription": "I'm here to help you turn your ideas into life.",
     "siteFavicon": "/favicon.svg",
